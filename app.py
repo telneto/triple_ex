c779e4ff4f5e405813636ca8f764adce9b765c53
telneto <ltelneto@gmail.com>
2026-05-21 16:27:19 -0500

    cambios en ubu

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
+st.write('Esta aplicación aún no es funcional y está en construcción.')
